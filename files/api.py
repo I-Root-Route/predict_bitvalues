d814259595e8abb625cf25197ed93f419193f1d4
I-Root-Route <68576038+I-Root-Route@users.noreply.github.com>
2020-09-11 11:26:04 +0900

    Update api.py

--- files/api.py
+++ files/api.py
@@ -1,11 +1,10 @@
 import datetime
 
-apiKey = 'A70FAF6E-FACE-4227-920F-7C93E130F6D3'
+apiKey = 'ご自身でcoinAPIなどのサイトでAPIキーを取得してください(無料でできます)'
 consURL = 'https://rest.coinapi.io/v1/ohlcv/'
 slash = '/'
 time = datetime.datetime.now().isoformat()
 
-#https://rest.coinapi.io/v1/ohlcv/BTC/JPY/history?period_id=1MIN&time_start=2016-01-01T00:00:00&apikey=A70FAF6E-FACE-4227-920F-7C93E130F6D3
 
 def get_coin_data(crypto, currency):
     request_url = consURL + crypto + slash + currency + slash + 'history?period_id=1DAY&time_end=' + time + '&apikey=' + apiKey
